2a76fe6761c87a4992c7012610021e9d1c1549ad
SashMashedpotatoesquashed-padhi <89152863+SashMashedpotatoesquashed-padhi@users.noreply.github.com>
2022-07-26 18:02:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+video = "";
+
+function setup() {
+    canvas = createCanvas(380, 350);
+    canvas.center();
+    video = createCapture(VIDEO); 
+    video.size(380,350); 
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
